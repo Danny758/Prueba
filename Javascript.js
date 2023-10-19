15231f43b628d2583da0b7b0561077e025cffedc
Danny758 <134745430+Danny758@users.noreply.github.com>
2023-10-19 10:43:17 -0400

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,10 @@
+respuesta = confirm("¿Quieres salir de la pagina?");
+respuesta2 = confirm("¿Seguro que quieres salir de la pagina?");
+respuesta3 = confirm("¿No mano pero seguro?");
+
+console.log(respuesta);
+console.log(respuesta2);
+console.log(respuesta3);
+
+
+alert("No se puede salir de la pagina xd")
